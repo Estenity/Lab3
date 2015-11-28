14dd3586e5b649a7e300b9114ef8ef7f5217330a
Estenity <skorohod_003@mail.ru>
2015-11-28 22:59:52 +0300

    Пункты 12-14 в peregryz.cpp все перегрузки minmax.cpp новая функция для мин, макс и среднего

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main()
 {
     using namespace std;
     Temperature data {0,K};
+    Temperature minimum {0,K};
+    Temperature maximum {0,K};
+    Temperature average {0,K};
     vector <Temperature> temp_tran;
     while (cout << "Please enter the temperature = ",
             cin >> data)
@@ -14,7 +17,6 @@ int main()
             temp_tran.push_back(Temperature(convert(data.value,data.symbol,K),K));
         }
         catch (const invalid_argument& e)
-
         {
             cerr << e.what() << endl;
         }
@@ -26,8 +28,14 @@ int main()
     cout << "       Translation Table      "<< endl;
     cout << fixed;
     cout.precision(1);
+    ofstream output ("file.txt");
     for (Temperature x:temp_tran)
     {
         cout << "Kelvin = " << x.value<< "\t Celsius = " << x.value-273.15 <<"\t Fahrenheit = " << ((x.value-273.15)*1.8+32) <<endl;
+        output << "Result: " << x << '\n';
+
     }
+    cout << endl;
+    minimax(temp_tran,minimum,maximum,average);
+    cout << "Minimum=" << minimum << "\t Maximum=" << maximum <<"\t Average=" << average/temp_tran.size() <<endl;
 }
